2bdec29d2032e3d0ae996a1c96ff442791d636c6
kordi0 <korolevdmitrii@gmail.com>
2022-03-29 22:19:42 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\webdriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        WebElement input = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+
+        input.click();
+    }
+}
