af4a72ad28642e10a6ce13c8ee3b7a1cf5f1cbbf
kordi0 <korolevdmitrii@gmail.com>
2022-03-31 22:24:10 +0300

    Явные и неявные ожидания

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\webdriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+        //неявное ожидание - устаревший метод
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //неявное ожидание - современный метод
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //явное 10 с. ожидание элемента с id=123
+        WebElement element = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("123"))));
         driver.get("https://google.com");
 
-        WebElement input = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
 
-        input.click();
-        //github test
     }
 }
